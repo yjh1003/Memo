adf9b459cb7b633febc37e146b2b358d058577a7
JIZIM\ckfwl <d>
2023-02-08 03:26:38 +0900

    [허예진] - Spring Project Memo 최초파일 추가

---
+++ src/test/java/com/yjh/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
