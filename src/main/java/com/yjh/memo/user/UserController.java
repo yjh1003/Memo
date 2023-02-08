6d04cf38032bdc63bdfaff915ad320b2215c1467
JIZIM\ckfwl <d>
2023-02-08 18:10:53 +0900

    [허예진] - 회원가입 화면 구성 - 회원가입 api

---
+++ src/main/java/com/yjh/memo/user/UserController.java
@@ -0,0 +1,16 @@
+package com.yjh.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/user/signup/view")
+	public String signupView() {
+		return "user/signup";
+		
+	}
+	
+	
+}
---
+++ src/main/java/com/yjh/memo/user/bo/UserBO.java
@@ -0,0 +1,24 @@
+package com.yjh.memo.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yjh.memo.user.dao.UserDAO;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	public int addUser(
+			String loginId
+			, String password
+			, String name
+			, String email) {
+		
+		return userDAO.insertUser(loginId, password, name, email);
+		
+	}
+
+}
